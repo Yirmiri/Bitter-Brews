8605f7e630a9f719b9df21f62386bec76da4c28d
yirmiri <yirmirilucy@gmail.com>
2024-05-25 14:34:58 -0400

    Initial commit

---
+++ src/main/java/net/yirmiri/BitterBrews.java
@@ -0,0 +1,16 @@
+package net.yirmiri;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BitterBrews implements ModInitializer {
+	public static final String MODID = "bitter_brews";
+    public static final Logger LOGGER = LoggerFactory.getLogger("bitter_brews");
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
