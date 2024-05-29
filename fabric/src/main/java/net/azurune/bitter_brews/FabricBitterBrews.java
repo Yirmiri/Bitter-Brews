e1ed5b9a84e22170a85e884c26577d9a1b0e4221
KekeCreations <89083260+KekeCreations@users.noreply.github.com>
2024-05-29 17:32:29 +0100

    Item Datagen

--- fabric/src/main/java/net/azurune/bitter_brews/FabricBitterBrews.java
+++ fabric/src/main/java/net/azurune/bitter_brews/FabricBitterBrews.java
@@ -6,13 +6,6 @@ public class FabricBitterBrews implements ModInitializer {
     
     @Override
     public void onInitialize() {
-        
-        // This method is invoked by the Fabric mod loader when it is ready
-        // to load your mod. You can access Fabric and Common code in this
-        // project.
-
-        // Use Fabric to bootstrap the Common mod.
-        BitterBrewsConstants.LOG.info("Hello Fabric world!");
         BitterBrews.init();
     }
 }
--- forge/src/main/java/net/azurune/bitter_brews/ForgeBitterBrews.java
+++ forge/src/main/java/net/azurune/bitter_brews/ForgeBitterBrews.java
@@ -1,9 +1,18 @@
 package net.azurune.bitter_brews;
 
+import net.azurune.bitter_brews.datagen.client.BitterBrewsItemModelProvider;
 import net.azurune.bitter_brews.platform.ForgeBitterBrewsRegistryHelper;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.PackOutput;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+import java.util.concurrent.CompletableFuture;
 
 @Mod(BitterBrewsConstants.MOD_ID)
 public class ForgeBitterBrews {
@@ -16,5 +25,28 @@ public class ForgeBitterBrews {
 
         ForgeBitterBrewsRegistryHelper.BLOCKS.register(modEventBus);
         ForgeBitterBrewsRegistryHelper.ITEMS.register(modEventBus);
+
+        modEventBus.addListener(this::gatherData);
+    }
+
+    @SubscribeEvent
+    public void gatherData(GatherDataEvent event) {
+        DataGenerator generator = event.getGenerator();
+        PackOutput packOutput = generator.getPackOutput();
+        ExistingFileHelper fileHelper = event.getExistingFileHelper();
+        CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
+
+        //Client
+        //generator.addProvider(event.includeClient(), new ArtsAndCraftsLangProvider(packOutput, ArtsAndCrafts.MOD_ID));
+        generator.addProvider(event.includeClient(), new BitterBrewsItemModelProvider(packOutput, BitterBrewsConstants.MOD_ID, fileHelper));
+        //generator.addProvider(event.includeClient(), new ArtsAndCraftsBlockStateProvider(packOutput, ArtsAndCrafts.MOD_ID, fileHelper));
+
+        //Server
+        //generator.addProvider(event.includeServer(), new ArtsAndCraftsRecipeProvider(packOutput));
+        //ArtsAndCraftsBlockTagsProvider blockTagProvider = new ArtsAndCraftsBlockTagsProvider(packOutput, lookupProvider, ArtsAndCrafts.MOD_ID, fileHelper);
+        //generator.addProvider(event.includeServer(), blockTagProvider);
+       //generator.addProvider(event.includeServer(), new ArtsAndCraftsItemTagsProvider(packOutput, lookupProvider, blockTagProvider.contentsGetter(), ArtsAndCrafts.MOD_ID, fileHelper));
+        //generator.addProvider(event.includeServer(), new ArtsAndCraftsLootTableProvider(packOutput));
+        //generator.addProvider(event.includeServer(), new ArtsAndCraftsBiomeTagsProvider(packOutput, lookupProvider, ArtsAndCrafts.MOD_ID, fileHelper));
     }
 }
