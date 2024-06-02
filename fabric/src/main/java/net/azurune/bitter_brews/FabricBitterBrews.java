8d5428703fab3248886a8bda476d242f92090e3a
KekeCreations <89083260+KekeCreations@users.noreply.github.com>
2024-06-02 01:52:56 +0100

    Coffee Bush World Generation

--- fabric/src/main/java/net/azurune/bitter_brews/FabricBitterBrews.java
+++ fabric/src/main/java/net/azurune/bitter_brews/FabricBitterBrews.java
@@ -1,8 +1,13 @@
 package net.azurune.bitter_brews;
 
+import net.azurune.bitter_brews.core.registry.BBFeatures;
 import net.azurune.bitter_brews.registry.FabricBBItemGroups;
 import net.azurune.bitter_brews.util.BBLootModifiers;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.minecraft.tags.BiomeTags;
+import net.minecraft.world.level.levelgen.GenerationStep;
 
 public class FabricBitterBrews implements ModInitializer {
     
@@ -11,5 +16,10 @@ public class FabricBitterBrews implements ModInitializer {
         BitterBrews.init();
         BBLootModifiers.modifyLoot();
         FabricBBItemGroups.buildCreativeTabs();
+        createBiomeModifications();
+    }
+
+    public void createBiomeModifications() {
+        BiomeModifications.addFeature(BiomeSelectors.tag(BiomeTags.IS_JUNGLE), GenerationStep.Decoration.VEGETAL_DECORATION, BBFeatures.PlacedFeatures.COFFEE_BUSH_PLACED);
     }
 }
