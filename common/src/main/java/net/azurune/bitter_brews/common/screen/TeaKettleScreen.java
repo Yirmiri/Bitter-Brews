ecd20f352279914228ee8a1add71f7b6fc99a0dc
KekeCreations <89083260+KekeCreations@users.noreply.github.com>
2024-06-08 15:04:06 +0100

    Build fix + removed useless files

--- common/src/main/java/net/azurune/bitter_brews/common/screen/TeaKettleScreen.java
+++ common/src/main/java/net/azurune/bitter_brews/common/screen/TeaKettleScreen.java
@@ -8,8 +8,6 @@ import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
-import net.minecraft.world.inventory.ClickType;
-import net.minecraft.world.inventory.Slot;
 
 public class TeaKettleScreen extends AbstractContainerScreen<TeaKettleMenu> {
     private static final ResourceLocation GUI_TEXTURE = new ResourceLocation(BitterBrewsConstants.MOD_ID, "textures/gui/tea_kettle.png");
