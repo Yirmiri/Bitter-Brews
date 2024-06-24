ad666b617f2688d06b99e61531758904c1cbb437
yirmiri <yirmirilucy@gmail.com>
2024-06-24 17:42:59 -0400

    yikes

--- common/src/main/java/net/azurune/bitter_brews/common/item/FuelSlotItem.java
+++
@@ -1,9 +0,0 @@
-package net.azurune.bitter_brews.common.item;
-
-import net.minecraft.world.item.Item;
-
-public class FuelSlotItem extends Item {
-    public FuelSlotItem(Properties properties) {
-        super(properties);
-    }
-}
